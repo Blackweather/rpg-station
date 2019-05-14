27ccbf61aa8ea6a30122d6dae00b262a4c93b138
Blackweather <szczepanski.karol0@gmail.com>
2019-05-14 22:53:29 +0200

    Add run-game script
    * run given game using given retroarch core
    * force fullscreen

---
+++ src/scripts/run-game.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# this scripts run the specified rom file
+# with a specified libretro core
+# example usage: ./run-game.sh gambatte ../../rom/gb/adjustris.gb 
+
+# TODO: change the path to standard path on raspbian
+libretro_path='/usr/lib/x86_64-linux-gnu/libretro/'
+core_extension='_libretro.so'
+core_path="$libretro_path$1$core_extension"
+
+retroarch -f -L $core_path $2
