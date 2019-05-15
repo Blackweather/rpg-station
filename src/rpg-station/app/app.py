65c4232450166dd85440cc5a898119651d4c858e
Blackweather <szczepanski.karol0@gmail.com>
2019-05-15 15:10:38 +0200

    Create app and gui submodules
    * add App class to hold the application
    * run the application in runner script

---
+++ src/rpg-station/app/app.py
@@ -0,0 +1,5 @@
+class App:
+    def run(self):
+        print("I'm now running the application")
+        while True:
+            pass
--- src/rpg-station/runner.py
+++ src/rpg-station/runner.py
@@ -1,1 +1,10 @@
+from app.app import App
 # This file serves as an entrypoint to the application
+
+def main():
+    print("This is the entrypoint to the application")
+    app = App()
+    app.run()
+
+if __name__ == "__main__":
+    main()
