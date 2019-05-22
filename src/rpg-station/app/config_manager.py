8cb62bbacb78ed34282a34966968956ba36c3f66
Blackweather <szczepanski.karol0@gmail.com>
2019-05-22 14:36:53 +0200

    Add platform with extensions loading
    * add get_platform_shorts_with_ext to ConfigManager
    * create empty GameManager class for managing ROM directory structure

--- src/rpg-station/app/config_manager.py
+++ src/rpg-station/app/config_manager.py
@@ -11,9 +11,17 @@ class Platforms:
         for p in self.platforms:
             names.append(p["platformName"])
         return names
+    def get_platform_shorts_with_ext(self):
+        # returns a dictionary
+        # platformshort : extensions
+        result = {}
+        for p in self.platforms:
+            result[p["platformShort"]] = p["extensions"]
+        return result
+
     #TODO: create methods for changing/adding platforms and overwriting the config
 
 # Sample usage
 plats = Platforms()
 plats.load_config()
-print(plats.get_platform_names())
+print(plats.get_platform_shorts_with_ext())
---
+++ src/rpg-station/app/game_manager.py
@@ -0,0 +1,8 @@
+# this class takes care of sorting the ROMs
+class GameManager:
+    def get_extensions(self):
+        pass
+    def load_from_dir(self, directory):
+        # get the directory
+        # copy appropriate extensions to appropriate dirs in the rom dir
+        pass
