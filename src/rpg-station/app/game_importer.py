962d2962ac1ac246bfd3ae765b816e4ecd6542cd
Blackweather <szczepanski.karol0@gmail.com>
2019-08-09 11:26:41 +0200

    Add import from filepath

--- src/rpg-station/app/game_importer.py
+++ src/rpg-station/app/game_importer.py
@@ -1,6 +1,7 @@
 from tkinter import Tk
 from tkinter import filedialog
 from . import game_manager
+import os
 
 class GameImporter:
     def import_from_picked_dir(self):
@@ -11,11 +12,18 @@ class GameImporter:
             gm.load_from_dir(directory)
 
     def import_from_picked_file(self):
+        #TODO: fix logs here?
         Tk().withdraw()
         filename = filedialog.askopenfilename()
         if filename != None:
             gm = game_manager.GameManager()
             gm.load_from_file(filename)
+        else:
+            print("Wrong file picked")
 
     def import_from_file(self, filename):
-        pass
+        if os.path.exists(filename):
+            gm = game_manager.GameManager()
+            gm.load_from_file(filename)
+        else:
+            print("Invalid path to file")
