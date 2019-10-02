a672df03d421ba56ef70891dc89893f47c548bc6
Blackweather <szczepanski.karol0@gmail.com>
2019-10-02 22:43:38 +0200

    Update script for joystick testing
    * add hat testing
    * add axis testing

--- src/scripts/jstest.py
+++ src/scripts/jstest.py
@@ -9,12 +9,16 @@ try:
     while True:
         for event in pygame.event.get():
             if event.type == pygame.JOYBUTTONDOWN:
-                buttons = j.get_numbuttons()
-                for i in range(buttons):
-                    if j.get_button(i):
-                        system('clear')
-                        print("Pressed button " + str(i))
-                        break
+                system('clear')
+                print("Pressed button " + str(event.button))
+            elif event.type == pygame.JOYHATMOTION:
+                system('clear')
+                print("Moved hat #" + str(event.hat) + " with value: "
+                      + str(event.value))
+            elif event.type == pygame.JOYAXISMOTION:
+                system('clear')
+                print("Moved axis #" + str(event.axis) + " with value: "
+                      + str(event.value))
 except KeyboardInterrupt:
     print("\nexiting now")
     j.quit()
