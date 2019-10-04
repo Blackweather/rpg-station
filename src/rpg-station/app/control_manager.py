2bbabccb99316c093e979362c699583d5138149e
Blackweather <szczepanski.karol0@gmail.com>
2019-10-04 18:41:47 +0200

    Update control_manager to return values from platform config file

--- src/rpg-station/app/control_manager.py
+++ src/rpg-station/app/control_manager.py
@@ -1,6 +1,13 @@
 from . import config
+import os.path
+import shutil
 
 class ControlManager:
+    # TODO: this class should use the template to get option names
+    # and actual config for option values
+    def __init__(self, platform):
+        self._platform = platform
+
     def load_template(self):
         # loads the template .conf as dictionary list
         template_path = config.RPG_ROOT + "/src/config/controls/template/config.cfg"
@@ -9,32 +16,40 @@ class ControlManager:
             template_content = config_template.readlines()
         # strip whitespace characters and remove spaces
         template_content = [x.strip().replace(' ', '') for x in template_content]
+
+        # determine actual config
+        platform_config_path = config.RPG_ROOT + "/src/config/controls/" + self._platform + "/config.cfg"
+
+        if not os.path.exists(platform_config_path):
+            shutil.copyfile(template_path, platform_config_path)
+
+        platform_config_content = []
+        with open(platform_config_path) as platform_config:
+            platform_config_content = platform_config.readlines()
+
+        platform_config_content = [x.strip().replace(' ', '') for x in platform_config_content]
+
+        platform_dict = {}
+        for line in platform_config_content:
+            if '=' in line:
+                config_value = line.split('=')
+                platform_dict[config_value[0]] = config_value[1]
         
         # transform to dictionary
-        template_dict = {}
+        result_dict = {}
         for line in template_content:
-            if line[0] != '#':
+            if line[0] != '#' and "joypad_index" not in line:
                 config_value = line.split('=')
-                template_dict[config_value[0]] = config_value[1]
+                result_dict[config_value[0]] = platform_dict[config_value[0]]
 
-        return template_dict
-
-    def filter_joypad_indexes(self, text):
-        JOYPAD_INDEX_TEXT = "joypad_index"
-
-        if JOYPAD_INDEX_TEXT in text:
-            return False
-        else:
-            return True
+        return result_dict
 
     def get_configurable_inputs_with_values(self):
         loaded_options = self.load_template()
         template_options = list(loaded_options.keys())
-        # filter out joypad index options
-        usable_options = filter(self.filter_joypad_indexes, template_options)
         # add values to filtered options
         result = {}
-        for option in usable_options:
+        for option in template_options:
             result[option] = loaded_options[option]
         return result
 
